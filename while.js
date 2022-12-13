f34991dde3fb6c4428b26cfe02298870d7a308ce
herlinFrondEnd <gamegoodml@gmail.com>
2022-12-13 16:25:47 +0800

    mengubah isi file operator html dan menambahkan file baru while.js"
    q
    clear
    
    :q!
    exit
    help

---
+++ while.js
@@ -0,0 +1,15 @@
+// pegulangan while
+
+// var ulang = true;
+// while(ulang) {
+// 	console.log('hello world');
+// 	ulang = confirm('lagi?');
+// }
+
+
+
+var nilaiAwal = 1;
+while(nilaiAwal <= 5) {
+	console.log('Hello world! ' + nilaiAwal + 'X');
+nilaiAwal++;
+}
