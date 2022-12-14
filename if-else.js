983bbfd1469375a8f7b618e36a530a699d5dee62
herlinFrondEnd <gamegoodml@gmail.com>
2022-12-14 18:16:58 +0800

    menambahkan fie if-else.js dan mengubah file index.html

---
+++ if-else.js
@@ -0,0 +1,9 @@
+var angka = prompt('masukkan angka:');
+
+if( angka % 2 == 0 ) {
+	alert(angka + ' adalahh bilanngan GENAP');
+} else if (angka % 2 == 1) {
+	alert(angka + ' adalah bilangn GANJIL');
+} else {
+	alert(angka + ' yang anda masukkan bukan angka!');
+}
