93237860086a2502f718e5809d80122aa24212ce
herlinFrondEnd <gamegoodml@gmail.com>
2022-12-15 17:32:44 +0800

    mengubah index.html dan menambahkan new file switch.js

---
+++ switch.js
@@ -0,0 +1,72 @@
+// var angka = parseInt(prompt('masukkan angka'));
+
+// if (angka === 1) {
+// 	alert('anda memasukkan angka 1');
+// } else if (angka === 2) {
+// 	alert('anda memasukkan angka 2'); 
+// } else {
+// 	alert('angka yanganda masukkan salah');
+// }
+
+
+
+
+
+// var angka = parseInt(prompt('masukkan angka :'));
+
+// switch(angka){
+// 	case 1 :
+// 		alert('anda memasukkan angka 1');
+// 		break;
+// 	case 2 :
+// 		alert('anda memasukkan angka 2');
+// 		break;
+// 	case 3 :
+// 		alert('anda memasukkan angka 3');
+// 		break;
+// 	default :
+// 		alert('angka yang anda masukkan salah');
+// }
+
+
+
+
+
+
+
+
+
+var item = prompt('masukkan nama makanan / minuman : \n (cth: nasi, daging,susu, hamburger, softdrink)')
+
+switch(item) {
+	case 'nasi' :
+	case 'daging' :
+	case 'susu' :
+		alert('makanan / minuman SEHAT!');
+		break;
+	case 'hamburger' :
+		alert('makanan / minuman KURANG SEHAT!');
+		break;
+	case 'softdrink' :
+		alert('makanan / minuman KURANG SEHAT!');
+		break;
+	default :
+		alert('makanan / minuman yang anda masukkan salah!');
+		break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
