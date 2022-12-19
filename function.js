20e8b7ef602a67566b1b7c40ae7f49526555d319
herlinFrondEnd <gamegoodml@gmail.com>
2022-12-19 14:03:37 +0800

    memodifikasi file fucntion,js

--- function.js
+++ function.js
@@ -26,11 +26,64 @@
   
 
 
-function tambah(a, b) {
-	return a + b;
+// function tambah(a, b) {
+// 	return a + b;
+// }
+
+// var a = parseInt(prompt('Masukkan nilai 1 :'));
+// var b = parseInt(prompt('Masukkan nilai 2 :'));
+// var hasil = tambah(a,b);
+// console.log(hasil);
+
+
+
+
+// function tambah(a,b) {
+// 	return a + b;
+// }
+
+// function kali(a,b) {
+// 	return a * b;
+// }
+
+
+// var hasil = kali(tambah(1,2), tambah(3,4));
+// console.log(hasil);
+
+
+
+
+
+
+// function tambah() {
+// 	return arguments;
+// }
+
+// var coba = tambah(5,10,20,"hi",false);
+// console.log(coba);
+
+
+
+
+
+
+
+function tambah(){
+	var hasil = 0;
+	for( var i = 0; i < arguments.length; i++ ) {
+		hasil += arguments[i];
+	}
+
+	return hasil;
 }
 
-var a = parseInt(prompt('Masukkan nilai 1 :'));
-var b = parseInt(prompt('Masukkan nilai 2 :'));
-var hasil = tambah(a,b);
-console.log(hasil);
+console.log(tambah(1,2,3,4,5));
+
+
+
+
+
+
+
+
+
