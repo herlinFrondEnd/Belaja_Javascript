79430726e48d0fb0c7ad2ad4c3c067277f822089
herlinFrondEnd <gamegoodml@gmail.com>
2022-12-20 12:42:20 +0800

    modifikasi file function.js

--- function.js
+++ function.js
@@ -118,13 +118,13 @@
 // tes(10);
 
 
-function faktorial(n) {
-	if ( n === 0 ) return 1;
-	return n * faktorial(n - 1);
-}
+// function faktorial(n) {
+// 	if ( n === 0 ) return 1;
+// 	return n * faktorial(n - 1);
+// }
 
-var hasil = faktorial(5);
-console.log(hasil);
+// var hasil = faktorial(5);
+// console.log(hasil);
 
 
 
